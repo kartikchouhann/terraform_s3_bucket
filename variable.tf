88cb9b9c9760165d2dd0fb8ad2319ee57df85013
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2022-09-06 15:15:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,12 +11,12 @@ variable "name" {
 
 variable "aws_access_key" {
   type    = string
-  default = "AKIA4XRVUBTP4JC4LHB6"
+  default = "AKIA4XRVUBHGYHGHGTP4JC4LHB6"
 }
 
 variable "aws_secret_key" {
   type    = string
-  default = "qVQk3UwkV7f3As41AXYfdUhhMMGs+/WwKX6TzqjX"
+  default = "qVQk3UwkV7f3As41AXYfdUhhMMGstghfdrjjhfhgfug+/WwKX6TzqjX"
 }
 
 variable "aws_region" {
